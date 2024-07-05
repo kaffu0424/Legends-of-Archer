b9859560ff1c9f43dd1bdceba2b976b6efb40da0
kaffu0424 <kaffu@naver.com>
2024-07-05 14:34:07 +0900

    조이스틱, 플레이어, 카메라
    1. 조이스틱 기능 완료
    2. 플레이어 이동 기능 완료
    3. 카메라 이동 기능 완료
    
    - 플레이어 애니메이션 bool타입과 enum을 사용해서 애니메이션이 전환되도록 수정

---
+++ Assets/JoyStickMovement.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyStickMovement : MonoBehaviour
+{
+    public static JoyStickMovement Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<JoyStickMovement>();
+                if(instance == null)
+                {
+                    var instanceContainer = new GameObject("joystickMovement");
+                    instance = instanceContainer.AddComponent<JoyStickMovement>();
+                }
+            }
+            return instance;
+        }
+    }
+    private static JoyStickMovement instance;
+
+
+    public GameObject smallStick;
+    public GameObject bgStick;
+
+    Vector3 stickFirstPosition;
+    Vector3 joyStickDefalutPosition;
+    public Vector3 joyVec;
+    float stickRadius;
+
+    private void Start()
+    {
+        stickRadius = bgStick.GetComponent<RectTransform>().sizeDelta.y / 2;
+        joyStickDefalutPosition = bgStick.transform.position;
+    }
+
+    public void PointDown()
+    {
+        bgStick.SetActive(true);                                
+        bgStick.transform.position = Input.mousePosition;       
+        smallStick.transform.position = Input.mousePosition;
+        stickFirstPosition = Input.mousePosition;
+
+        PlayerMovement.Instance.WalkPlayer();
+    }   
+
+    public void Drag(BaseEventData baseEventData)
+    {
+        PointerEventData pointerEvemtData = baseEventData as PointerEventData;
+
+        Vector3 dragPosition = pointerEvemtData.position;
+        joyVec = (dragPosition - stickFirstPosition).normalized;
+
+        float stickDistance = Vector3.Distance(dragPosition, stickFirstPosition);
+
+        if (stickDistance < stickRadius)
+            smallStick.transform.position = stickFirstPosition + joyVec * stickDistance;
+        else
+            smallStick.transform.position = stickFirstPosition + joyVec * stickRadius;
+    }
+
+    public void Drop()
+    {
+        joyVec = Vector3.zero;
+        bgStick.transform.position = joyStickDefalutPosition;
+        smallStick.transform.position = joyStickDefalutPosition;
+
+        PlayerMovement.Instance.IdlePlayer();
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,101 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerAnimatorState
+{
+    IDLE,
+    WALK,
+    DAMEGE,
+    ATTACK
+}
+
+public class PlayerMovement : MonoBehaviour
+{
+    public static PlayerMovement Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<PlayerMovement>();
+                if (instance == null)
+                {
+                    var instanceContainer = new GameObject("playerMovement");
+                    instance = instanceContainer.AddComponent<PlayerMovement>();
+                }
+            }
+            return instance;
+        }
+    }
+    private static PlayerMovement instance;
+
+    private Rigidbody playerRB;
+    [SerializeField, Range(0, 50)] private float moveSpeed;
+
+    public Animator playerAnimator;
+
+    private PlayerAnimatorState playerState;
+    string[] stateStrings;
+
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
+
+        playerState = PlayerAnimatorState.IDLE;
+        stateStrings = new string[4];
+        stateStrings[(int)PlayerAnimatorState.IDLE] = "IDLE";
+        stateStrings[(int)PlayerAnimatorState.WALK] = "WALK";
+        stateStrings[(int)PlayerAnimatorState.DAMEGE] = "DAMEGE";
+        stateStrings[(int)PlayerAnimatorState.ATTACK] = "ATTACK";
+    }
+
+    private void FixedUpdate()
+    {
+        if (JoyStickMovement.Instance.joyVec.x != 0 || JoyStickMovement.Instance.joyVec.y != 0)
+        {
+            playerRB.velocity = new Vector3(JoyStickMovement.Instance.joyVec.x * moveSpeed, playerRB.velocity.y, JoyStickMovement.Instance.joyVec.y * moveSpeed);
+
+            playerRB.rotation = Quaternion.LookRotation(new Vector3(
+                JoyStickMovement.Instance.joyVec.x ,0 ,JoyStickMovement.Instance.joyVec.y));
+        }
+    }
+
+    public void IdlePlayer()
+    {
+        playerRB.velocity = Vector3.zero;
+        ChangeState(PlayerAnimatorState.IDLE);
+        // setTrigger Idle
+    }
+
+    public void WalkPlayer()
+    {
+        playerAnimator.SetTrigger("WALK");
+        ChangeState(PlayerAnimatorState.WALK);
+    }
+
+    public void ChangeState(PlayerAnimatorState state)
+    {
+        playerAnimator.SetBool("IDLE", false);
+        playerAnimator.SetBool("WALK", false);
+        playerAnimator.SetBool("ATTACK", false);
+
+        playerState = state;
+        switch(state)
+        {
+            case PlayerAnimatorState.IDLE:
+                playerAnimator.SetBool("IDLE", true);
+                break;
+
+            case PlayerAnimatorState.WALK:
+                playerAnimator.SetBool("WALK", true);
+                break;
+
+            case PlayerAnimatorState.ATTACK:
+                playerAnimator.SetBool("ATTACK", true);
+                break;
+        }
+    }
+}
